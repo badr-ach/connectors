4f310cfca2d802f46b7cd4182d459b8b077b36c8
root <root@instance-1.us-central1-a.c.citric-nova-407207.internal>
2023-12-05 20:32:18 +0000

    update : version

---
+++ mongo-sink-connector/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
@@ -0,0 +1,9 @@
+package com.mongodb.kafka.connect;
+
+public final class Versions {
+  public static final String NAME = "mongo-kafka";
+
+  public static final String VERSION = "1.11.0-3-g62fedf0-dirty";
+
+  private Versions() {}
+}
