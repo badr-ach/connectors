35d8661d993c590062d9b40fa9c282b2f3fa732c
root <root@instance-1.us-central1-a.c.citric-nova-407207.internal>
2023-12-06 14:12:43 +0000

    update configs

--- mongo-sink-connector/build/spotless/spotlessJava/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
+++ mongo-sink-connector/build/spotless/spotlessJava/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
@@ -3,7 +3,7 @@ package com.mongodb.kafka.connect;
 public final class Versions {
   public static final String NAME = "mongo-kafka";
 
-  public static final String VERSION = "1.11.0-3-g62fedf0-dirty";
+  public static final String VERSION = "aeca8e-dirty";
 
   private Versions() {}
 }
