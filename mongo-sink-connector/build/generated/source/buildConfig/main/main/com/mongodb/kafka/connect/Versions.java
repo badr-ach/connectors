34721fe42b6f76d2e4340302df6fc9e18954ebaa
root <root@instance-1.us-central1-a.c.citric-nova-407207.internal>
2023-12-06 23:28:35 +0000

    add : redis connector

--- mongo-sink-connector/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
+++ mongo-sink-connector/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
@@ -3,7 +3,7 @@ package com.mongodb.kafka.connect;
 public final class Versions {
   public static final String NAME = "mongo-kafka";
 
-  public static final String VERSION = "da9a51-dirty";
+  public static final String VERSION = "e187e9-dirty";
 
   private Versions() {}
 }
