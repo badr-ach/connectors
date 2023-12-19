2314d3aea779dd1884e714dbf5fc804db2487b09
root <root@instance-1.us-central1-a.c.citric-nova-407207.internal>
2023-12-19 20:47:32 +0000

    remove : dupes

--- mongo-sink-connector/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
+++
@@ -1,9 +0,0 @@
-package com.mongodb.kafka.connect;
-
-public final class Versions {
-  public static final String NAME = "mongo-kafka";
-
-  public static final String VERSION = "e187e9-dirty";
-
-  private Versions() {}
-}
