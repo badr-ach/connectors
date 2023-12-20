5cb86c9e7e44a7e595bf61c0669e834102017f68
root <root@instance-1.us-central1-a.c.citric-nova-407207.internal>
2023-12-20 20:13:39 +0000

    update : stuff

--- mongo-sink-connector/build/spotless/spotlessJava/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
+++ mongo-sink-connector/build/spotless/spotlessJava/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
@@ -3,7 +3,7 @@ package com.mongodb.kafka.connect;
 public final class Versions {
   public static final String NAME = "mongo-kafka";
 
-  public static final String VERSION = "e187e9-dirty";
+  public static final String VERSION = "862c92-dirty";
 
   private Versions() {}
 }
--- mongo-sink-connector/docker/run.sh
+++ mongo-sink-connector/docker/run.sh
@@ -71,21 +71,6 @@ test_systems_available 8085
 
 trap clean_up EXIT
 
-echo -e "\nConfiguring the MongoDB ReplicaSet.\n"
-docker-compose exec mongo1 /usr/bin/mongo --eval '''if (rs.status()["ok"] == 0) {
-    rsconf = {
-      _id : "rs0",
-      members: [
-        { _id : 0, host : "mongo1:27017", priority: 1.0 },
-        { _id : 1, host : "mongo2:27017", priority: 0.5 },
-        { _id : 2, host : "mongo3:27017", priority: 0.5 }
-      ]
-    };
-    rs.initiate(rsconf);
-}
-
-rs.conf();'''
-
 echo -e "\nKafka Topics:"
 curl -X GET "http://localhost:8082/topics" -w "\n"
 
