cee96fa210ecedb425f6adb89923fb933cb903c8
badr-ach <alachkarbadr@gmail.com>
2024-01-03 23:06:57 +0100

    update : two redis

--- mongo-sink-connector/build/spotless/spotlessJava/build/generated/source/buildConfig/main/main/com/mongodb/kafka/connect/Versions.java
+++
@@ -1,9 +0,0 @@
-package com.mongodb.kafka.connect;
-
-public final class Versions {
-  public static final String NAME = "mongo-kafka";
-
-  public static final String VERSION = "cb86c9-dirty";
-
-  private Versions() {}
-}
